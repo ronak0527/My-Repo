7bb597ff633cb1394af42ca4b60e7b11408e2215
ronak0527 <100175973+ronak0527@users.noreply.github.com>
2022-06-06 21:03:23 -0400

    Add files via upload

---
+++ JS-Stock-Charts/public/index.js
@@ -0,0 +1,9 @@
+async function main() {
+
+    const timeChartCanvas = document.querySelector('#time-chart');
+    const highestPriceChartCanvas = document.querySelector('#highest-price-chart');
+    const averagePriceChartCanvas = document.querySelector('#average-price-chart');
+
+}
+
+main()
